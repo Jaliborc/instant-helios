c44055e3bfd71dbc00b2329cf1b914675f11fd50
Joao Cardoso <jaliborc@arenajunkies.com>
2018-10-26 02:14:45 +0100

    Now tests ensure current directory agnostic and only stream files

--- index.js
+++ index.js
@@ -74,9 +74,12 @@ exports.js = function(out) {
 }
 
 exports.assets = function(out) {
-  fs.readdirSync(__dirname + '/assets').forEach(file => {
-    fs.createReadStream(path.join(__dirname, '/assets', file)).pipe(fs.createWriteStream(path.join(out, file)))
-    console.log(`${success} Written ${file}`)
+  fs.readdirSync(__dirname + '/assets', { withFileTypes: true }).forEach(file => {
+    let input = path.join(__dirname, '/assets', file)
+    if (fs.statSync(input).isFile()) {
+      fs.createReadStream(input).pipe(fs.createWriteStream(path.join(out, file)))
+      console.log(`${success} Written ${file}`)
+    }
   })
 }
 
